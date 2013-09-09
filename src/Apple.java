63380d6b72bf3306fb92e83fdae81e25150b3378
Dmitry Stabrovsky <dmitry.stabrovsky@gmail.com>
2013-09-09 17:41:57 +0300

    Init commit

---
+++ src/Apple.java
@@ -0,0 +1,26 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: dimka
+ * Date: 9/9/13
+ * Time: 5:37 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Apple {
+    public int getSeedCount() {
+        return seedCount;
+    }
+
+    public void setSeedCount(int seedCount) {
+        this.seedCount = seedCount;
+    }
+
+    private int seedCount;
+
+    public Apple() {
+        seedCount = (int)(Math.random() * 10);
+    }
+
+    public Apple (int count) {
+        seedCount = count;
+    }
+}
